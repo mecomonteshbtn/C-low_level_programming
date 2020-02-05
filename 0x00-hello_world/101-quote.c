08b25629dab8f74bb8344cb462de56aa68607810
Robinson Montes <1574@holbertonschool.com>
2020-02-05 21:23:50 +0000

    Correction on file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
