70098ba4365c7dd1e76818c8c1efcdd9414d6519
Robinson Montes <1574@holbertonschool.com>
2020-02-05 21:29:10 +0000

    Correction on file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -6,6 +7,7 @@
  * Return: Always 1 (Success)
  */
 
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
