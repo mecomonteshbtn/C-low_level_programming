c3b2fe77cfdb0a666587b6a6b268813a802d21cc
Robinson Montes <1574@holbertonschool.com>
2020-02-06 20:11:25 +0000

    correction excercise 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,11 @@
 
 int main(void)
 {
-        char low;
+	char low;
 
-        for (low = 'z'; low >= 'a'; low--)
-                putchar(low);
-        putchar('\n');
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
 
-        return (0);
+	return (0);
 }
-
