1195c4e8cf78feb77e1876eb8487ffc71757e093
Robinson Montes <1574@holbertonschool.com>
2020-02-06 21:00:02 +0000

    First advanced excercise

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int d;
+	int i, j;
 
-	for (d = 0; d < 90; d++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for ()
+		for (j = i+1; j <= '9'; j++)
 		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
-		{
+			putchar(i);
+			putchar(j);
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
