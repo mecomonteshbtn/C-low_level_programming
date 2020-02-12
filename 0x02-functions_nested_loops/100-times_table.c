95a0a1e6e9fc2b98a16deea55dd0c599f401b141
Robinson Montes <1574@holbertonschool.com>
2020-02-12 01:10:49 +0000

    holberton: actualized

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,15 +9,13 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	i = 0;
 	if (n > 0 && n < 15)
 	{
-		for (; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			j = 1;
-			for (; j <= n; j++)
-				putformat(i * j++);
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
 			_putchar('\n');
 		}
 	}
