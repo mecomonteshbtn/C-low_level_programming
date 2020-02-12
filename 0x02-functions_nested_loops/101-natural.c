60e7c1ba3a582be57003b09d1489828e5448c9a2
Robinson Montes <1574@holbertonschool.com>
2020-02-12 01:27:14 +0000

    Project 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("Sum = %d\n", sum);
+
+	return (0);
+}
