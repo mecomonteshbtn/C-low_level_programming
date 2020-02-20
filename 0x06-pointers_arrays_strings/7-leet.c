83261190d5058a1d784474011134287293e6b111
Robinson Montes <1574@holbertonschool.com>
2020-02-20 20:56:30 +0000

    Project 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * leet - a function that encodes a string into 1337.
+ * @s: an input string to encode
+ * Return: An encode string
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+
+		i++;
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,3 +5,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+char *leet(char *s);
