18a35cf11033f48d900db1e6182e2c5193852429
Robinson Montes <1574@holbertonschool.com>
2020-02-20 20:58:14 +0000

    Corrections on Project 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,16 +14,16 @@ char *leet(char *s)
 		if (s[i] == 'a' || s[i] == 'A')
 			s[i] = '4';
 
-		else if (s[i] == 'e' || s[i] == 'E')
+		if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
 
-		else if (s[i] == 'o' || s[i] == 'O')
+		if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
 
-		else if (s[i] == 't' || s[i] == 'T')
+		if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
 
-		else if (s[i] == 'l' || s[i] == 'L')
+		if (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
 
 		i++;
