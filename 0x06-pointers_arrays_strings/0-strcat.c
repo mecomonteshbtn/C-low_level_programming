67824df3828688f3ad2bb83116db5a43c1b62ff0
Robinson Montes <1574@holbertonschool.com>
2020-02-20 18:27:40 +0000

    Project 0, 1 and 2

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: an input string
+ * @src: an input string
+ * Return: A pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,3 @@
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
