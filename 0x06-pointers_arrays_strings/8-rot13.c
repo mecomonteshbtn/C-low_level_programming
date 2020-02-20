57789856804e678d5d651400a8caa189ed6aa5ac
Robinson Montes <1574@holbertonschool.com>
2020-02-20 22:38:42 +0000

    Project 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -14,11 +14,11 @@ char *rot13(char *s)
 		while ((s[i] >= 'a' && s[i] <= 'z') ||
 				(s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if ((s[i] >= 'N' && s[i] <= 'Z') ||
-					(s[i] >= 'n' && s[i] <= 'z'))
-				s[i] -= 13;
-			else
+			if ((s[i] >= 'a' && s[i] <= 'm') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
 				s[i] += 13;
+			else
+				s[i] -= 13;
 			i++;
 		}
 		i++;
