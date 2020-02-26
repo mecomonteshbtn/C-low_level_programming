f4d0fce085000e0a7eb341c61033800c56e75736
Robinson Montes <1574@holbertonschool.com>
2020-02-26 19:02:53 +0000

    Project 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,13 @@
 #include "holberton.h"
 
 /**
- *  - a function that prints a string, followed by a new line.
+ * _strlen_recursion - a function that returns the length of a string.
  * @s: An input string to printing
- * Return: Nothing
+ * Return: The length of the string
  */
 int _strlen_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
 	if (*s)
-		_puts_recursion(s);
-	else
-		_putchar('\n');
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
