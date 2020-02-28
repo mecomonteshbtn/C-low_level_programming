4d3569ab9b776d1464411aa019ccd85516f87d9c
Robinson Montes <1574@holbertonschool.com>
2020-02-28 09:34:03 +0000

    last projects

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * wildcmp - A function that compares two strings
+ * @s1: First input string
+ * @s2: Second input string
+ * Return: 1 if strings identical and 0 in otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,18 +1,53 @@
 #include "holberton.h"
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
+int helper_palindrome(char *s, int len);
 
 /**
- * is_palindrome - a function that checks if s is palindrome
- * @s: An input string
- * Return: 0 if s is palindrome or 0 if s isn't
+ *_strlen_recursion - A function to obtain the length of the string s
+ * @s: A string to calculate length
+ * Return: the length of string s
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palindrome - A function that checks if s is a palindrome string
+ * @s: An inpuit string
+ * Return: 1 if is string is a palindrome or 0 in otherwise
  */
 int is_palindrome(char *s)
 {
-	int len = 0;
+	int len;
 
-	if (*s)
+	len = _strlen_recursion(s);
+	if (len <= 1)
+		return (1);
+	return (helper_palindrome(s, len));
+}
+
+/**
+ * helper_palindrome - A function with a revesed string
+ * @s: An input string
+ * @len: the length of the string s
+ * Return: A reverse string
+ */
+int helper_palindrome(char *s, int len)
+{
+	if (len <= 1)
+		return (1);
+	else if (*s == *(s + len - 1))
 	{
-		len++;
-		is_palindrome(s + 1);
+		return (helper_palindrome(s + 1, len - 2));
 	}
-	return(check_palindrome(s, len));
+	else
+		return (0);
+
 }
