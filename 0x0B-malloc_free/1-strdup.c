a15b386ff691cac5cc11abc1058168654a0f2f7c
Robinson Montes <1574@holbertonschool.com>
2020-03-03 20:34:47 +0000

    corrections on 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -28,16 +28,16 @@ char *_strdup(char *str)
 	new_str = malloc(sizeof(char) * (len + 1));
 	start = new_str;
 
-	if (new_str == NULL)
-		return (NULL);
-	else
+	if (new_str != NULL)
 	{
 		for (; i < len; i++)
-	{		
+		{
 			new_str[i] = *str;
 			str++;
 		}
 		new_str[i] = '\0';
 		return (start);
 	}
+	else
+		return (NULL);
 }
