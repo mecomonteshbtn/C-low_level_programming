9ee95911b8f283991e77d2627579850c6d1bec38
Robinson Montes <1574@holbertonschool.com>
2020-03-05 17:47:46 +0000

    Project 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - A function that allocates memory for an array using malloc
+ * @nmemb: number of memory spaces
+ * @size: size of nmemb
+ * Return: returns pointer to allocated space, or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *space;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	space = malloc(nmemb * size);
+	if (space == NULL)
+		return (NULL);
+
+	for (; i < size; i++)
+		*(space + (size * i)) = 0;
+
+	return (space);
+}
