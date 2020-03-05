7bed456f0e2cb1f8f8672c7607912aac65ec375e
Robinson Montes <1574@holbertonschool.com>
2020-03-05 17:49:46 +0000

    Corrections on Project 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (space == NULL)
 		return (NULL);
 
-	for (; i < size; i++)
+	for (; i <= size; i++)
 		*(space + (size * i)) = 0;
 
 	return (space);
