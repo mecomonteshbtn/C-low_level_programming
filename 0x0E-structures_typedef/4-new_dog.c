1203059c66330b934ce21f164f73c91f8c38e025
Robinson Montes <1574@holbertonschool.com>
2020-03-09 21:31:33 +0000

    project 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,6 +14,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cpyname, *cpyowner;
 	int len_name = 0, len_owner = 0, i;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	while (name[len_name])
 		len_name++;
 	while (owner[len_owner])
