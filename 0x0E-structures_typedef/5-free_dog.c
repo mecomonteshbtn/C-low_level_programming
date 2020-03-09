39c81489f6554b9cdcbd286aea1272f5d7b729ab
Robinson Montes <1574@holbertonschool.com>
2020-03-09 19:36:02 +0000

    project 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,11 +8,11 @@
  */
 void free_dog(dog_t *d)
 {
-  if (d == NULL)
-    return;
-  if (d->name != NULL)
-    free(d->name);
-  if (d->owner != NULL)
-    free(d->owner);
-  free(d);
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
 }
