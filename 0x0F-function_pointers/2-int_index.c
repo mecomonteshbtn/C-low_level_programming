180acc386d576101b779a2f529df0f366225473e
Robinson Montes <1574@holbertonschool.com>
2020-03-11 21:06:00 +0000

    sort like holberton

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (cmp == NULL || array == NULL)
+		return (-1);
+
 	if (size <= 0)
 		return (-1);
 
-	if (cmp != NULL || array != NULL)
-	{
-		for (; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
-	}
+	for (; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
 
 	if (i == size)
 		return (-1);
