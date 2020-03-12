8d7c9f80be8c7422e81c2f501c6cdf779a0f3fcf
Robinson Montes <1574@holbertonschool.com>
2020-03-12 02:05:40 +0000

    project 100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints opcode of own main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 1 or 2 on fail, 0 on success
+ */
+int main(int argc, char *argv[])
+{
+  int bytes, i;
+  unsigned char *func_ptr;
+
+  if (argc != 2)
+    {
+      printf("Error\n");
+      exit(1);
+    }
+  bytes = atoi(argv[1]);
+  if (bytes < 0)
+    {
+      printf("Error\n");
+      exit(2);
+    }
+  func_ptr = (unsigned char *)main;
+  i = 0;
+  if (bytes > 0)
+    {
+      while (i < (bytes - 1))
+	printf("%02hhx ", func_ptr[i++]);
+      printf("%hhx\n", func_ptr[i]);
+    }
+  return (0);
+}
