31586128ef1df7fe4a627ed29e06f712216b8864
Robinson Montes <1574@holbertonschool.com>
2020-03-17 01:40:27 +0000

    double inclusion

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -43,7 +43,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
+	{
+		printf("Error");
 		exit(100);
+	}
 
 	return (a / b);
 }
@@ -57,6 +60,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
+	{
 		exit(100);
+		printf("Error");
+	}
 	return (a % b);
 }
