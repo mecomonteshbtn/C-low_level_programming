48474359517ba6d09d76bb3974ed5dd05fbfa480
Robinson Montes <1574@holbertonschool.com>
2020-03-19 17:23:55 +0000

    corrections on project 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
+/* Using constructors in C */
+void main_constructor(void) __attribute__((constructor));
 /**
  * premain - A function that prints a message before main runs
  * Return: Nothing
  */
-void __attribute__((constructor)) premain();
+void main_constructor(void);
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,7 +20,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-int _putchar(char c);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
