2a02b411dd65258d4bcde768bf06de7ceb1bd155
Robinson Montes <1574@holbertonschool.com>
2020-03-24 20:44:18 +0000

    Some Changes in project 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,29 +12,37 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 1;
 	listint_t *new_node, *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-	temp = *head;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		new_node->n = n;
+		return (new_node);
+	}
 
 	if (idx == 0)
 	{
 		new_node->next = *head;
+		new_node->n = n;
 		*head = new_node;
 		return (new_node);
 	}
 
+	temp = *head;
 	while (i < idx)
 	{
 		temp = temp->next;
 		i++;
 	}
+
+	new_node->n = n;
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
