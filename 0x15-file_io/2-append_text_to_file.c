c2a49cdd280588e15eaae69d4fa8dff476a1028a
Robinson Montes <1574@holbertonschool.com>
2020-03-31 23:16:27 +0000

    Optimizing 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,26 +13,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, error, len = 0;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd < 0)
+	fdo = open(filename, O_RDWR | O_APPEND);
+	if (fdo < 0)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(fd);
+		close(fdo);
 		return (1);
 	}
 
 	while (*(text_content + len))
 		len++;
 
-	error = write(fd, text_content, len);
-	close(fd);
-	if (error < 0)
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
 		return (-1);
 
 	return (1);
