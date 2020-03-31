8b3022d2eab9800350dc693f3a3acd70199ac854
Robinson Montes <1574@holbertonschool.com>
2020-03-31 17:51:26 +0000

    Corrections on task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,8 +7,6 @@
 #include <stdlib.h>
 
 int safe_close(int);
-void write_error(char *);
-void read_error(char *);
 /**
  * main - Main function to copy files
  * @argc: The number of passed arguments
@@ -29,14 +27,14 @@ int main(int argc, char *argv[])
 	from_fd = open(argv[1], O_RDONLY);
 	if (from_fd < 0)
 	{
-		read_error(argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (to_fd < 0)
 	{
-		write_error(argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		safe_close(from_fd);
 		exit(99);
 	}
@@ -46,7 +44,7 @@ int main(int argc, char *argv[])
 		_EOF = read(from_fd, buffer, 1024);
 		if (_EOF < 0)
 		{
-			read_error(argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			safe_close(from_fd);
 			safe_close(to_fd);
 			exit(98);
@@ -57,7 +55,7 @@ int main(int argc, char *argv[])
 		error = write(to_fd, buffer, _EOF);
 		if (error < 0)
 		{
-			write_error(argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			safe_close(from_fd);
 			safe_close(to_fd);
 			exit(99);
@@ -88,24 +86,4 @@ int safe_close(int description)
 	if (error < 0)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", description);
 	return (error);
-}
-
-/**
- * read_error - A function that prints a read error message
- * @read_output_error: The file to print output error
- * Return: Nothing
- */
-void read_error(char *read_output_error)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", read_output_error);
-}
-
-/**
- * write_error - A function that prints a write error message
- * @write_output_error: The file to print output error
- * Return: Nothing
- */
-void write_error(char *write_output_error)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", write_output_error);
 }
