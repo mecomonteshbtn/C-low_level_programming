67487d42d6e8d2cc7218ed8f7853fc2c93820e75
Robinson Montes <1574@holbertonschool.com>
2020-03-31 23:07:48 +0000

    file 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point for program to get header of ELF file
- * @argc: count of arguments present in CLI
- * @argv: pointer to array of pointers to arguments
- *
+ * main - The entry point for program to get header of ELF file
+ * @argc: The number of arguments
+ * @argv: The pointer to array of arguments
  * Return: 1 on success, error code on failure
  */
 int main(int argc, char *argv[])
 {
-  printf("argc:%d, argv:%p\n", argc, (void *)*argv);
-  return (1);
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
 }
