97261c4983b0d233a22eab442677fe4d877755a0
Robinson Montes <1574@holbertonschool.com>
2020-03-31 23:03:22 +0000

    Optimizing 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,21 +13,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, error, len = 0;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd < 0)
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
 		return (-1);
 
 	while (text_content && *(text_content + len))
 		len++;
 
-	error = write(fd, text_content, len);
-	close(fd);
-	if (error < 0)
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
 		return (-1);
 	return (1);
 }
