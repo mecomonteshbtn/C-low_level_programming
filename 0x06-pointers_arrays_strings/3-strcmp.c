8e6958d0118f839f27032045685f84644accaa77
Robinson Montes <1574@holbertonschool.com>
2020-04-21 21:25:54 +0000

    fix strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - a function that compares two strings.
  * @s1: an input string
  * @s2: an input string
- * Return: The difference between 
+ * Return: The difference s1 - s2
  */
 int _strcmp(char *s1, char *s2)
 {
