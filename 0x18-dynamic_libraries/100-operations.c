f629ef89d13e364079cfd0204d7f5dac0375afea
Robinson Montes <1574@holbertonschool.com>
2020-05-05 01:12:23 +0000

    task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,24 @@
+int add(int a, int b)
+{
+  return (a + b);
+}
+
+int sub(int a, int b)
+{
+  return (a - b);
+}
+
+int div(int a, int b)
+{
+  return (a / b);
+}
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
+int mod(int a, int b)
+{
+  return (a % b);
+}
