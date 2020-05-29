678b2b195d59798b2fb38abe5a9fb091e6ebb220
Robinson Montes <1574@holbertonschool.com>
2020-05-29 21:53:40 +0000

    attempt 100

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,11 +44,11 @@ typedef struct hash_table_s
  */
 typedef struct shash_node_s
 {
-  char *key;
-  char *value;
-  struct shash_node_s *next;
-  struct shash_node_s *sprev;
-  struct shash_node_s *snext;
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
 } shash_node_t;
 
 /**
@@ -63,10 +63,10 @@ typedef struct shash_node_s
  */
 typedef struct shash_table_s
 {
-  unsigned long int size;
-  shash_node_t **array;
-  shash_node_t *shead;
-  shash_node_t *stail;
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
 } shash_table_t;
 
 
