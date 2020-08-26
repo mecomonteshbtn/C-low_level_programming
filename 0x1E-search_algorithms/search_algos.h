bcf00449c874cf15cbf76d5c569f62b60c8ad6f7
Robinson Montes <1574@holbertonschool.com>
2020-08-26 17:18:46 +0000

    The header file with guards and functions for project

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
