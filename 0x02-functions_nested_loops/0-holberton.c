d56dc0a7aeb8c11a7d8f82bf8694fe97112248bc
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-03-15 09:08:53 +0300

    My first

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
+ * Description: It prints the word Putchar followed by a new line.
  * Return: 0.
  */
 int main(void)
